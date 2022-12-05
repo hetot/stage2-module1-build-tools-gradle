05d7c5af17eacf1ec0b01c055f090a3cd0a707f8
hetot <shaxzod.murtazayev@mail.ru>
2022-12-05 06:08:40 +0300

    try3

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,8 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
+            if (str.charAt(0) == '0' && str.length() > 1)
+                return false;
             return Double.parseDouble(str) > 0;
         } catch (Exception e) {
             return false;
