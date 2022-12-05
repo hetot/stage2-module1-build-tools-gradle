87ec5328e355cde75d93d98f9057c06b95fd4128
hetot <shaxzod.murtazayev@mail.ru>
2022-12-05 05:52:28 +0300

    try2

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,12 +1,20 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if (args == null || args.isEmpty())
+            return false;
         for (String num : args) {
-            if (Integer.parseInt(num) < 0)
+            try {
+                if (StringUtils.isPositiveNumber(num))
+                    return false;
+            } catch (Exception e) {
                 return false;
+            }
         }
         return true;
     }
